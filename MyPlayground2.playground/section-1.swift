160fd5a712ded5b9f7a1c5d4dd9bb4e750a2ec5c
E.L <eladlavi@Elads-MacBook-Pro.local>
2015-01-26 20:50:21 +0200

    Older stuff

---
+++ MyPlayground2.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground";
+//str = 5; //this is an error because 5 is not a String.
+str = "Hello HackerU";
+let s1:String = "Hello,  playground";
+//s1 = "Hello HackerU"; //this is an error because s1 is a constant.
+var s2:String = "Hello HackerU";
+s2 = "Good evening";
+//common Swift types: String, Int, Float, Double, Bool
+let s:String = "Hi there";
+let i:Int = -25;
+let f:Float = 3.14;
+let d:Double = 99.99;
+let b:Bool = true;
+
