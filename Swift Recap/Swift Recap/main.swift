b2a2856786c6f887f63adcb85f96da69e63df6dd
E.L <eladlavi@Elads-MacBook-Pro.local>
2015-02-05 17:59:55 +0200

    recap

---
+++ Swift Recap/Swift Recap/main.swift
@@ -0,0 +1,52 @@
+//
+//  main.swift
+//  Swift Recap
+//
+//  Created by Elad Lavi on 2/5/15.
+//  Copyright (c) 2015 Elad Lavi. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+//1. Playground, Hello World, println
+//2. Constants and Variables.
+//3. Comments.
+//4. Numbers.
+//5. Strings.
+//6. Booleans.
+//7. Tuples.
+//8. Optionals.
+//9. Type aliases
+//10. Global variables vs. Local Variables.
+//11. Assignment (Value Types vs. Reference Types)
+//12. Math operators
+//13. Compersion operators
+//14. Range operators (..<)
+//15. Logical operatos (&& || !)
+//16. Enums
+//17. Arrays and Dictionaries
+//18. Loops and if statement.
+//19. Functions (and nested functions)
+//20. Closures (sending a block of code as a parameter). Also called: anonymous functions.
+//21. Structures
+//22. Classes
+//23. instance equality operator (=== and !==)
+//24. Properties (get and set).
+//    two kinds of properties: stored and calculated.
+//    we also learned about Lazy.
+//25. Property Obeservers (willSet and didSet) (not important)
+//26. Class types properties (static properties)
+//27. Class Methods.
+//28. Subscripts  ([])  (not important)
+//29. Inheritance    (:)
+//30. Overriding methods and properties
+//31. Initializing an object.
+
+//Closures:
+
+
+
+
+
