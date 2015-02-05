bc2596b2230ab0c81761f039741262d2ff3bbdbc
E.L <eladlavi@Elads-MacBook-Pro.local>
2015-02-05 20:59:46 +0200

    extensions

--- Swift Recap/Swift Recap/main.swift
+++ Swift Recap/Swift Recap/main.swift
@@ -80,9 +80,26 @@ println(rect.description());
 
 var _counter = 0;
 class Person{
-    var name: String = "John";
-    var age:Int = 0;
+    var name: String;
+    var age:Int;
     private var _lastName: String = ""; //private means: visible only within this file!
+    
+    
+    init(name: String, age: Int){
+        self.name = name;
+        self.age = age;
+    }
+    
+    convenience init(){
+        self.init(name: "John", age: 0);
+        
+        //self.name = "John";
+        //self.age = 0;
+        
+    }
+
+    
+
     
     class var counter:Int{
         return _counter;
@@ -100,6 +117,24 @@ class Person{
     
     func description() ->String{
         return "I'm \(self.name) and I'm \(self.age) years old.";
+    }
+    
+    func doPersonThings(){
+        println("\(self.name) is doing person things...");
+    }
+    
+    deinit{
+        println("in person deinit");
+    }
+    
+    
+    class Health {
+        var pulse:Int = 100;
+        var bmi:Int = 20;
+        
+        func isHealthy() ->Bool{
+            return (pulse>80 && bmi<30);
+        }
     }
 }
 
@@ -139,4 +174,86 @@ println(matt3 !== matt);
 
 
 //inheritance
+class Employee: Person{
+    var employeeNumber:Int = 1234567890;
+    var hourlyRate:Float = 100.00;
+    
+    
+    init(name:String, age:Int, employeeNumber:Int, hourlyRate:Float){
+        super.init(name: name, age: age);
+        self.employeeNumber = employeeNumber;
+        self.hourlyRate = hourlyRate;
+    }
+
+    init(){
+        super.init(name: "John",age: 0);
+    }
+
+    override func description() -> String {
+        
+        
+        return "\(super.description()) My employee # is \(self.employeeNumber) and my hourly rate is: \(self.hourlyRate).";
+    }
+    
+    func doEmployeeThings(){
+        println("\(self.name) is doing employee things...");
+    }
+}
+var emp1:Employee = Employee();
+emp1.name = "Jim";
+emp1.age = 18;
+emp1.employeeNumber = 1;
+emp1.hourlyRate = 80.0;
+println(emp1.description());
+
+var person2:Person? = Person();
+println(person2!.description());
+person2 = nil;
+
+//casting:
+let d1 = 9.1234567;
+let i1 = Int(d1);
+let f1 = Float(d1);
+println(d1);
+println(f1);
+let b1 = Bool(d1);
+println(b1);
+let s1 = toString(d1);
+
+
+var e1 = Employee();
+e1.name = "Jim";
+e1.doPersonThings();
+e1.doEmployeeThings();
+
+
+let a1:[Person] = [e1];
+
+for item in a1{
+    item.doPersonThings();
+}
+
+class Animal{
+    
+}
+
+class Girl : Person{
+    
+}
+var g = Girl();
+let e2 = g as Person; //upcasting
+//let e3 = e2 as Employee; //downcasting can cause runtime error.
+//e3.doEmployeeThings();
+
+//extensions (like inheritance, but for specific scope)
+
+extension Person{
+    func bark(){
+        println("waf waf");
+    }
+}
+
+e2.bark();
+
+
 
